ae68a910a034b31162058c1edf76a655cd6118c9
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-18 23:25:47 +0100

    Adds ejs and sets it as the templating engine. Creates a songs route and a songs.ejs file. The data is passed through to the songs view and then rendered

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res) {
     res.send("Testing, testing, 123")
-
 });
+
+app.get("/songs", function(req, res) {
+    const songs = [
+        {title: "Spirit", artist: "Beyonce Knowles"},
+        {title: "Bad Guy", artist: "Billie Eilish"},
+        {title: "Juice", artist: "Lizzo"},
+        {title: "Dance Monkey", artist: "Tones and I"},
+        {title: "I Like It", artist: "Cardi B"}
+    ];
+     res.render("songs.ejs", {songs: songs})
+  });
 
 
 app.listen(3000, function() {
